1edbecd829a68504374b7717ac8926a92c5d122e
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-14 16:42:37 +0530

    initial project

---
+++ src/main/java/com/easybytes/easystore/EasystoreApplication.java
@@ -0,0 +1,13 @@
+package com.easybytes.easystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasystoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easybytes/easystore/EasystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybytes.easystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
