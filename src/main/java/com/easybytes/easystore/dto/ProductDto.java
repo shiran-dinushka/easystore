4f58ebc3c0f53547267c7ce82653e26fdfa3972d
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-16 16:46:53 +0530

    Add ProductDto

---
+++ src/main/java/com/easybytes/easystore/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package com.easybytes.easystore.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+public class ProductDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer popularity;
+    private String imageUrl;
+    private Instant createdAt;
+}
