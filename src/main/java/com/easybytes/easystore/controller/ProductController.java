1b0ca4317bf4cf961de99308102d6881fc53aadc
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-16 17:15:51 +0530

    Add ProductController

--- src/main/java/com/easybytes/easystore/controller/ProductController.java
+++ src/main/java/com/easybytes/easystore/controller/ProductController.java
@@ -1,15 +1,23 @@
 package com.easybytes.easystore.controller;
 
+import com.easybytes.easystore.dto.ProductDto;
+import com.easybytes.easystore.service.IProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final IProductService iProductService;
+
     @GetMapping
-    public String getProducts(){
-        return "Here are your products";
+    public List<ProductDto> getProducts(){
+        return iProductService.getProducts();
     }
 }
