9d51ee589b163157b2359d5f45eb9748daffd30c
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-28 20:10:50 +0530

    Add AuditorAwareImpl to get current auditor

--- src/main/java/com/easybytes/easystore/EasystoreApplication.java
+++ src/main/java/com/easybytes/easystore/EasystoreApplication.java
@@ -2,8 +2,10 @@ package com.easybytes.easystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class EasystoreApplication {
 
 	public static void main(String[] args) {
