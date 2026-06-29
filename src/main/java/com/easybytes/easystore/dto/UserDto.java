3f430766ecb19274d3e387c25a530b92956f2c05
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-29 20:03:30 +0530

    Add UserDto

---
+++ src/main/java/com/easybytes/easystore/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.easybytes.easystore.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+
+    private Long userId;
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
