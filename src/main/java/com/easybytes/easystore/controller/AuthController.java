e21a2b2c36c9417c2e06ff76709844c7bb0e40cb
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-29 20:06:59 +0530

    Implement login authentication

---
+++ src/main/java/com/easybytes/easystore/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.easybytes.easystore.controller;
+
+import com.easybytes.easystore.dto.LoginRequestDto;
+import com.easybytes.easystore.dto.LoginResponseDto;
+import com.easybytes.easystore.dto.UserDto;
+import com.easybytes.easystore.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> apiLogin(@RequestBody LoginRequestDto loginRequestDto) {
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequestDto.username(), loginRequestDto.password()));
+            var userDto = new UserDto();
+            var loggedInUser = (User) authentication.getPrincipal();
+            userDto.setName(loggedInUser.getUsername());
+            String jwtToken = jwtUtil.generateJwtToken(authentication);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new LoginResponseDto(HttpStatus.OK.getReasonPhrase(), userDto, jwtToken));
+        } catch(BadCredentialsException ex) {
+            return buildErrorResponse(HttpStatus.UNAUTHORIZED, "Invalid username or password");
+        } catch (AuthenticationException ex) {
+            return buildErrorResponse(HttpStatus.UNAUTHORIZED, "Authentication failed");
+        }catch (Exception ex) {
+            return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred");
+        }
+    }
+
+    private ResponseEntity<LoginResponseDto> buildErrorResponse(HttpStatus status, String message) {
+        return ResponseEntity.status(status).body(new LoginResponseDto(message, null, null));
+    }
+}
--- src/main/java/com/easybytes/easystore/security/EazyStoreSecurityConfig.java
+++ src/main/java/com/easybytes/easystore/security/EazyStoreSecurityConfig.java
@@ -3,8 +3,16 @@ package com.easybytes.easystore.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -34,6 +42,28 @@ public class EazyStoreSecurityConfig {
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user1 = User.builder().username("madan")
+                .password("$2a$12$EgH2Ft44EmnctjQ/6EuLqeJUKB.w6tzv.aPinRoCXpSzTnal/DCm6").build();
+        var user2 = User.builder().username("admin")
+                .password("$2a$12$MKPD0OTRIEl7KuPad.HLf.hPlKeVLzKfe5BCvRIJ0C9Ipm3F/esDi").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        var daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
