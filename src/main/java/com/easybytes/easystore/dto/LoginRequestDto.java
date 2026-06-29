0f63c1121bf4f696a4bfbf826954544c0f0450d0
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-29 20:04:44 +0530

    Add LoginRequestDto and LoginResponseDto

---
+++ src/main/java/com/easybytes/easystore/dto/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package com.easybytes.easystore.dto;
+
+public record LoginRequestDto(String username, String password) {
+}
---
+++ src/main/java/com/easybytes/easystore/dto/LoginResponseDto.java
@@ -0,0 +1,4 @@
+package com.easybytes.easystore.dto;
+
+public record LoginResponseDto(String message, UserDto user, String jwtToken) {
+}
